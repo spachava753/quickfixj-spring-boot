a9f2e1be0167488701db27f4e5dc788063f29512
spachava753 <spachava753@gmail.com>
2018-11-20 16:31:41 -0500

    First commit

---
+++ src/main/java/com/example/quickfixjspringboot/QuickfixjSpringBootExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.quickfixjspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickfixjSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickfixjSpringBootExampleApplication.class, args);
+	}
+}
